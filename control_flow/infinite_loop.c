e92a65231a5eea9a4da12e52284603cd8342d6a2
yusufcore <mohammadyusuf.work@gmail.com>
2026-07-28 14:07:28 +0000

    add infinite loop

---
+++ control_flow/infinite_loop.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){             // infinte loop.
+    for(;;){           
+        printf("Hello\n");
+    }
+}
+// for loop syntax -->  ; ;  are must. not even a single semi-colon can be missed else it will give an error.
