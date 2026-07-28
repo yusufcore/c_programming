c077ea9737e810483e9234f6389209c9c16fa2fb
yusufcore <mohammadyusuf.work@gmail.com>
2026-07-28 14:12:30 +0000

    add more ways to right a for_loop

--- control_flow/for_loop.c
+++ control_flow/for_loop.c
@@ -1,10 +1,61 @@
 #include <stdio.h>
 
-int main(){       // determinante loops
-    int i;
-    for(i = 0; i< 20; i++){     // for loop in C.
-        printf("Hello %dth time \n", i);
+// int main(){       // determinante loops
+//     int i;
+//     for(i = 0; i< 20; i++){     // for loop in C.
+//         printf("Hello %dth time \n", i);
 
-    }
-    return 0;
-}
+//     }
+//     return 0;
+// }
+
+
+// int main(){              // way 2 to write a for loop.
+//     int i = 0;
+//     for(;i<10;i++){
+//         printf("i=%d\n", i);
+//     }
+// }
+
+
+// int main(){             // way 3 to right a for loop.
+//     int i = 0;
+//     for(;i<10;){
+//         printf("i = %d\n", i);
+//         i++;
+//     }
+// }
+
+// int main(){             // way 4 to right a for loop.
+//     int i = 0;
+//     for(i=0;i<10;i++){
+//         printf("i=%d\n", i);
+//     }
+// }
+
+
+// int main(){
+//     int i;
+//     for(i=0;i<10;i*=2){
+//         printf("i=%d\n", i);
+//     }
+// }
+
+
+
+// int main(){
+//     int i;
+//     for(i=0;i<10;i++){     // second increment
+//         printf("i=%d\n", i);
+//         i++;     // first increment
+//     }
+// }
+
+
+// int main(){
+//     int i;
+//     for(i=0;i<10;i++){
+//         if(i==5)break;             // syntax.
+//         printf("i=%d\n", i);
+//     }
+// }   // break statement 
