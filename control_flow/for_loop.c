eab92d8de956c0a407ae6bfb1afe3bf877a37963
yusufcore <mohammadyusuf.work@gmail.com>
2026-07-28 14:01:25 +0000

    add for loop

---
+++ control_flow/for_loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){       // determinante loops
+    int i;
+    for(i = 0; i< 20; i++){     // for loop in C.
+        printf("Hello %dth time \n", i);
+
+    }
+    return 0;
+}
