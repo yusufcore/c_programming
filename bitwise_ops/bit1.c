0d2d08272826ad335e166597ce00298162d39202
yusufcore <mohammadyusuf.work@gmail.com>
2026-07-30 20:23:39 +0530

    add documentation

--- bitwise_ops/bit1.c
+++ bitwise_ops/bit1.c
@@ -1,28 +1,27 @@
-// Number of 1s in its binary representation
+// to find the number of 1s in the binary representation of a positive integer.
 
 #include <stdio.h>
 
-// int main(){              
+int main(){              
 
-//     unsigned int x;
-//     printf("Enter a positive integer: ");
-//     scanf("%u", &x);  
+    unsigned int x;
+    printf("Enter a positive integer: ");
+    scanf("%u", &x);  
 
-//     int count = 0;
-//     while(x!=0){
-//         if((x & 1) == 1){
-//             count ++;   // +1.
-//         }
-//         x = x >> 1;   // right shift.
-//     }
-//     printf("Number of ones => %d\n", count);
-//     return 0;
+    int count = 0;
+    while(x!=0){
+        if((x & 1) == 1){
+            count ++;   // +1.
+        }
+        x = x >> 1;   // right shift.
+    }
+    printf("Number of ones => %d\n", count);
+    return 0;
 
-// }
+}
 
 
-  
-// optimised way --> it reduces the number of operations to be performed.
+// // optimised way --> it reduces the number of operations to be performed.
 int main(){
 
     unsigned int x;
@@ -30,9 +29,9 @@ int main(){
     scanf("%u", &x);  
 
     int count = 0;
-    while(x){
+    while(x!=0){
+        x = x & (x-1);  
         count ++;   
-        x = x & (x-1);     
     }
     printf("Number of ones => %d\n", count);
     return 0;
