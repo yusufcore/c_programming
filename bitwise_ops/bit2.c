d4abb0c9b2e43c3e98b259ea3148b8d2876bac61
yusufcore <mohammadyusuf.work@gmail.com>
2026-07-30 11:44:06 +0530

    add bitwise operation

---
+++ bitwise_ops/bit2.c
@@ -0,0 +1,15 @@
+// to find if a number is in the power of 2 or not.
+
+#include <stdio.h>
+
+int main(){
+    unsigned int x;
+    printf("Enter an Integer: ");
+    scanf("%u", &x);
+    if((x&(x-1)) == 0){    // bitwise comparison.
+        printf("Number is in power of 2\n");
+    }else{
+        printf("Number is not in the power of 2\n");
+    } 
+    return 0;
+}
