d59950298cb4e120a04385852120bc0909832908
yusufcore <mohammadyusuf.work@gmail.com>
2026-08-02 22:35:15 +0530

    add escape sequence

---
+++ input_output/escape_sequence.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    int b = 1500;
+    float h = 200.50;
+    char g = 'A';
+    printf("Basic = %d\n, HRA = %f\n, Grade = %c\n", b,h,g);
+    return 0;   
+    // note -> \n is an escape sequence which is used to print the next output in a new line.
+}
