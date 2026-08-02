f2e1bdcdbaedec9627165f039c1ba5853e3eac06
yusufcore <mohammadyusuf.work@gmail.com>
2026-08-02 23:42:04 +0530

    add exercise on input/output

---
+++ input_output/exercise.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+
+    //(4) int a = 50000;
+    // unsigned int b = 50000;
+    // printf("a = %d, b = %u\n", a,b);
+    // return 0;
+
+    //(6) float a = 123.1265;
+    // printf("%f\t", a);    // prints 123.126500
+    // printf("%.2f\t", a);  // prints 123.13
+    // printf("%.3f\t", a);  // prints 123.127
+
+    //(7) int a = 625, b = 2394 , c = 12345;
+    // printf("%5d, %5d, %5d\n", a,b,c);
+    // printf("%3d, %4d, %5d\n", a,b,c);
+    // return 0;
+
+    //(8) int a=98;
+    // char ch='c';
+    // printf("%c, %d\n", a,ch);
+
+    //(9) float a1,b1,a2,b2,a3,b3;
+    // a1=2;
+    // b1=6.8;
+    // a2=4.2;
+    // b2=3.57;
+    // a3=9.82;
+    // b3=85.673;
+    // printf("%3.1f, %4.1f\n", a1,b1);
+    // printf("%5.1f,%6.2f\n", a2,b2);
+    // printf("%7.1f, %8.2f\n", a3,b3);
+    // return 0;
+
+    //(10) printf("%10s\n", "India");
+    // printf("%4s\n", "India");
+    // printf("%.2s\n", "India");
+    // printf("%5.2s\n", "India");
+    // return 0;
+}
