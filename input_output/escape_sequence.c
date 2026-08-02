006ff56a39a7829acd3cde45feb12037dbac2041
yusufcore <mohammadyusuf.work@gmail.com>
2026-08-02 22:37:48 +0530

    refactor escape sequence example and update output formatting

--- input_output/escape_sequence.c
+++ input_output/escape_sequence.c
@@ -2,10 +2,20 @@
 
 int main(){
 
+    // int b = 1500;
+    // float h = 200.50;
+    // char g = 'A';
+    // printf("Basic = %d\n, HRA = %f\n, Grade = %c\n", b,h,g);
+    // return 0;   
+    // // note -> \n is an escape sequence which is used to print the next output in a new line.
+   
+
     int b = 1500;
     float h = 200.50;
     char g = 'A';
-    printf("Basic = %d\n, HRA = %f\n, Grade = %c\n", b,h,g);
+    printf("Basic = %d\t HRA = %f\t Grade = %c\t", b,h,g);
     return 0;   
-    // note -> \n is an escape sequence which is used to print the next output in a new line.
+    
+    // notes -> \t is an escape sequence which is used to print the next output after a tab space.
+
 }
