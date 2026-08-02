e7ee635001227e83642b0442895fa56de7c31d9d
yusufcore <mohammadyusuf.work@gmail.com>
2026-08-02 22:27:57 +0530

    add hexadecimal representation in c

---
+++ input_output/hexa.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+
+    int num = 10;
+    printf("Hex equivalent of decimal %d = %x", num, num);  // prints 'a' (hexadecimal representation of 10)
+    return 0;
+}
