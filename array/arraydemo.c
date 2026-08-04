08c48d560b080d21b56bb6ebc899f45344440cd8
yusufcore <mohammadyusuf.work@gmail.com>
2026-08-04 17:57:08 +0530

    add array

---
+++ array/arraydemo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int arr[] = {5,7,9,11,2,6};
+    printf("%d\n", (int)sizeof(arr));  // gives 24.
+    printf("%d\n", (int)sizeof(arr[0]));   // gives 4 (which is the size of each element).
+    int length = sizeof(arr) / sizeof(arr[0]);
+    printf("length = %d\n", length);    // 24/4 -> 6(no. of elements in the current array);
+
+    return 0;
+}
