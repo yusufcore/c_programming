4b4cf60c9ccc10829836ab993eb6f09c63dd2c79
yusufcore <mohammadyusuf.work@gmail.com>
2026-08-06 22:11:20 +0530

    add user defined functions

---
+++ functions_c/func2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void drawline(void);  // function declaration.
+
+int main(){    
+    drawline();      // function call.
+}
+void drawline(void){    // function definition.
+    int i;
+    for(i=1; i<=80; i++){
+        printf("--");
+    }
+}
+
+
+int sum(int x, int y);  // function declaration.
+
+int main(){
+    int a,b,s;
+    printf("Enter the values of a and b: ");
+    scanf("%d %d", &a,&b);
+    s = sum(a,b);   // function calling.
+    printf("Sum of %d and %d is %d\n", a,b,s);
+}
+int sum(int x, int y){   // defining a function.
+    int s;
+    s = x + y;
+    return s;
+}
