36af32d29dff8f5b37010f2dd8769aba1156ba6b
yusufcore <mohammadyusuf.work@gmail.com>
2026-08-07 22:27:32 +0530

    add use of return statement

---
+++ functions_c/return.c
@@ -0,0 +1,23 @@
+// use of return statement.
+// 1) to terminate a function without returning any value.
+#include <stdio.h>
+void funct(int age, float ht);
+
+int main(){
+    int age;
+    float ht;
+    printf("Enter age and height: ");
+    scanf("%d %f", &age, &ht);
+    funct(age,ht);
+}
+void funct(int age, float ht){
+    if(age>25){
+        printf("Age should be less than 25\n");
+        return;
+    }
+    if(ht<5){
+        printf("Height should be more than 5\n");
+        return;
+    }
+    printf("Selected\n");
+}
