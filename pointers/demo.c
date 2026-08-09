aeffb788155c4212492430a8a3d6f743e111604b
yusufcore <mohammadyusuf.work@gmail.com>
2026-08-09 19:23:50 +0530

    add documentation

--- pointers/demo.c
+++ pointers/demo.c
@@ -10,6 +10,17 @@ int main(){
     printf("%d\n", *(&a));  // same result --> 9.
     printf("%f\n", *(&b));   // same result --> 10.07.
     printf("%.2f\n", *fptr);  // dereferencing  --> we get 10.07 as output.
-    
+    printf("\n");
+    printf("Address of a = %p\n", iptr);
+    printf("Address of b = %p\n", fptr);
+    printf("\n");
+    printf("Address of iptr = %p\n", &iptr);
+    printf("Address of fptr = %p\n", &fptr);
+    printf("\n");
+    printf("sizeof(a)  = %zu\n", sizeof(a));   // prints 4.
+    printf("sizeof(b)  = %zu\n", sizeof(b));   // prints 4;
+    printf("sizeof(iptr)  = %zu\n", sizeof(iptr));   // prints 8;
+    printf("sizeof(fptr)  = %zu\n", sizeof(fptr));   // prints 8;
+
     return 0;
 }  
