52ec2897f79ca67e1d323c895ebd7615398aea95
yusufcore <mohammadyusuf.work@gmail.com>
2026-08-09 19:12:01 +0530

    add dereferencing

--- pointers/demo.c
+++ pointers/demo.c
@@ -7,6 +7,8 @@ int main(){
     int *iptr = &a;  // pointer iptr contains the address of a.
     float *fptr = &b;
     printf("%d\n", *iptr);  // dereferencing  --> we get 9 as output.
+    printf("%d\n", *(&a));  // same result --> 9.
+    printf("%f\n", *(&b));   // same result --> 10.07.
     printf("%.2f\n", *fptr);  // dereferencing  --> we get 10.07 as output.
     
     return 0;
