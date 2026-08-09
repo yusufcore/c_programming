75109ebf3d5be0d302a01fb1362a8ba721bc51f9
yusufcore <mohammadyusuf.work@gmail.com>
2026-08-09 18:38:49 +0530

    add intro to pointers

---
+++ pointers/demo.c
@@ -0,0 +1,13 @@
+// Pointers
+#include <stdio.h>
+
+int main(){
+    int a = 9;
+    float b = 10.07;
+    int *iptr = &a;  // pointer iptr contains the address of a.
+    float *fptr = &b;
+    printf("%d\n", *iptr);  // dereferencing  --> we get 9 as output.
+    printf("%.2f\n", *fptr);  // dereferencing  --> we get 10.07 as output.
+    
+    return 0;
+}  
