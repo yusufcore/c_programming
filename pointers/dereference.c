3b10d889fad9ec9658039845a6fdd27e0e37af5f
yusufcore <mohammadyusuf.work@gmail.com>
2026-08-11 19:22:46 +0530

    add derefencing of pointers

---
+++ pointers/dereference.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void){
+    int a = 87;
+    float b = 4.5;
+    int *p1 = &a;
+    float *p2 = &b;
+
+    printf("Value of p1 = Memory address of a = %p\n", p1);
+    printf("Value of p2 = Memory address of b = %p\n", p2);
+    printf("Address of p1 = %p\n", &p1);
+    printf("Address of p2 = %p\n", &p2);
+    printf("value of a = %d %d %d\n", a, *p1, *(&a));
+    printf("value of a = %.1f %.1f %.1f\n", b, *p2, *(&b));
+    return 0;
+
+}
