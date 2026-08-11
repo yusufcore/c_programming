15823412411d18d67e1ac7a6100b23ed3eb1b1a5
yusufcore <mohammadyusuf.work@gmail.com>
2026-08-11 19:50:09 +0530

    add documentation

--- pointers/dereference.c
+++ pointers/dereference.c
@@ -9,8 +9,9 @@ int main(void){
     printf("Value of p2 = Memory address of b = %p\n", p2);
     printf("Address of p1 = %p\n", &p1);
     printf("Address of p2 = %p\n", &p2);
-    printf("value of a = %d %d %d\n", a, *p1, *(&a));
-    printf("value of a = %.1f %.1f %.1f\n", b, *p2, *(&b));
+    printf("value of a = %d %d %d\n", a, *p1, *(&a));  // dereferencing
+    printf("value of b = %.1f %.1f %.1f\n", b, *p2, *(&b));  // dereferencing
     return 0;
-
+ 
+    /*Note --> for dereferencing, * is needed. Dereferencing gets you the value of at that address, which is stored in pointer.*/
 }
